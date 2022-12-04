7a22d0fc7e79735db0ea6aa5b6bb8c36ec48e2d9
ANDY <Beanland412@gmail.com>
2022-12-04 21:38:02 +0800

    修正錯誤-1

--- js/all.js
+++ js/all.js
@@ -10,28 +10,14 @@ $(function(){
 		if($(window).scrollTop()>0)
 			$('.left,.right').addClass('center')
 	});
-	text('people',12700,13000)
-	text('join',200,500)
-	text('share',500,800)
-	function text(className,start,end){
-		var timer = setInterval(()=>{
-			start++
-			$('.'+className).text(start+"+");
-			if(start>=end){
-				clearInterval(timer)
-			}
-		})
-	};
-	$('.team li:first').addClass('bg-gray');
-	$('.team li').click(function(){
-		$(this).addClass('bg-gray').siblings().removeClass()
-	});
-	$('.pd-block:first').addClass('bg-green');
-	$('.pd-block').click(function(){
-		$(this).addClass('bg-green').siblings().removeClass('bg-green');
-		$('.alltext span').addClass('run');
-		setTimeout(()=>{
-			$('.alltext span').removeClass('run');
-		},1000)
-	});
 
+
+
+
+	$('.addToCart').click(function(){
+		$('.check').fadeIn();
+		setTimeout(()=>{
+			$('.check').fadeOut();
+		})
+	})
+})
